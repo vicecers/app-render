c72d328941677bee2cbe7abf75b36a765f9b57ed
vicente <vicecers@gmail.com>
2023-02-19 10:46:26 -0300

    manejo de errores con ResponseEntity<?>

--- src/main/java/com/cerso/alumno/controller/AlumnoController.java
+++ src/main/java/com/cerso/alumno/controller/AlumnoController.java
@@ -2,8 +2,13 @@ package com.cerso.alumno.controller;
 
 import com.cerso.alumno.entity.Alumno;
 import com.cerso.alumno.service.AlumnoService;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,36 +21,111 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class AlumnoController {
-    
+
     @Autowired
     AlumnoService alumnoService;
-    
+
     @GetMapping("/alumnos")
-    public List<Alumno> index(){
+    public List<Alumno> index() {
+
         return alumnoService.getAlumnos();
     }
-    
-    
+
     @GetMapping("/alumnos/{id}")
-    public Alumno show(@PathVariable Long id){
-        return alumnoService.findAlumnoById(id);
+    public ResponseEntity<?> show(@PathVariable Long id) {
+
+        Alumno alumno = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            alumno = alumnoService.findAlumnoById(id);
+        } catch (DataAccessException e) {
+            response.put("mensaje:", "Error de acceso a la base de datos");
+            response.put("error:", e.getMessage().concat("").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        if (alumno == null) {
+            response.put("mensaje", "El alumno no se encuentra en el sistema");
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Alumno>(alumno, HttpStatus.OK);
+        //return alumnoService.findAlumnoById(id);
     }
-    
+
     @PostMapping("/save")
-    public Alumno create(@RequestBody Alumno alumno){
-        return alumnoService.saveAlumno(alumno);
+    public ResponseEntity<?> create(@RequestBody Alumno alumno) {
+
+        Alumno alumnoNuevo = null;
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            alumnoNuevo = alumnoService.saveAlumno(alumno);
+
+        } catch (DataAccessException e) {
+            response.put("mensaje:", "Error al insertar el alumno a la base de datos");
+            response.put("error:", e.getMessage().concat("").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje:", "Alumno creado con exito");
+        response.put("alumno", alumnoNuevo);
+        return new ResponseEntity<Alumno>(alumno, HttpStatus.CREATED);
+        //return alumnoService.saveAlumno(alumno);
     }
-    
-    
+
     @PutMapping("/update/{id}")
-    public Alumno update(@PathVariable Long id, @RequestBody Alumno alumno){
-        return alumnoService.updateAlumno(id, alumno);
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Alumno alumno) {
+
+        Alumno alumnoActualizado = null;
+        Map<String, Object> response = new HashMap<>();
+
+        alumnoActualizado = alumnoService.findAlumnoById(id);
+
+        if (alumnoActualizado == null) {
+            response.put("mensaje", "No se ha podido actualizar al alumno porque no se encuentra en el sistema");
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        try {
+
+            alumnoActualizado = alumnoService.updateAlumno(id, alumno);
+
+        } catch (DataAccessException e) {
+
+            response.put("mensaje:", "Error al actualizar el alumno en la base de datos");
+            response.put("error:", e.getMessage().concat("").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+
+        }
+
+        response.put("mensaje", "alumno actualizado con exito");
+        response.put("alumno actualizado", alumnoActualizado);
+
+        return new ResponseEntity<Alumno>(alumnoActualizado, HttpStatus.CREATED);
+
+        //return alumnoService.updateAlumno(id, alumno);
     }
-    
-    
+
     @DeleteMapping("/delete/{id}")
-    public void delete(@PathVariable Long id){
-        alumnoService.deleteAlumno(id);
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+
+        Map<String, Object> response = new HashMap<>();
+        
+        try {
+            alumnoService.deleteAlumno(id);
+            
+        } catch (DataAccessException e) {
+            
+            response.put("mensaje:", "Error al eliminar el alumno de la base de datos");
+            response.put("error:", e.getMessage().concat("").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
+        response.put("mensaje:", "Alumno eliminado con exito");
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+
+        //alumnoService.deleteAlumno(id);
     }
-	
+
 }
