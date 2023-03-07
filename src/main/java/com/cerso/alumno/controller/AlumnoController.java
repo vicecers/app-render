de285581c0722f6ad944d540d6fb3d7c22ee56bf
vicente <vicecers@gmail.com>
2023-03-07 14:47:14 -0300

    modificacion application.properties gitignore rev02

--- src/main/java/com/cerso/alumno/controller/AlumnoController.java
+++ src/main/java/com/cerso/alumno/controller/AlumnoController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = {"*"})
 public class AlumnoController {
 
     @Autowired
---
+++ src/test/java/com/cerso/alumno/ApialumnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cerso.alumno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApialumnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
